fb584f5e0e1ad6b0fa270079ca0dcb5dd3f1589b
feron <avel.feron@gmail.com>
2022-02-06 18:40:07 +0100

    tt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { Header } from './components/Header'
+
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <div className="App">
+        <Header></Header>
       ssss
     </div>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+export const Header = () => {
+
+
+    return (
+        <div>
+            <AppBar>
+                test
+            </AppBar>
+        </div>
+    )
+
+};
+
